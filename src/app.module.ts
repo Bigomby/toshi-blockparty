58c0c41df5c6af4973c9b5036ae212ff438adf25
Diego Fernández Barrera <bigomby@gmail.com>
2018-03-19 19:44:26 +0100

    :sparkles: Import all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { ClientModule } from './modules/client/client.module';
 import { IdentityModule } from './modules/identity/identity.module';
 import { EthModule } from './modules/eth/eth.module';
 import { SessionModule } from './modules/session/session.module';
+import { WSModule } from './modules/ws/ws.module';
+import { WalletModule } from './modules/wallet/wallet.module';
 
 @Module({
-  imports: [ClientModule, IdentityModule, EthModule, SessionModule],
+  imports: [
+    ClientModule,
+    EthModule,
+    IdentityModule,
+    SessionModule,
+    WalletModule,
+    WSModule,
+  ],
 })
 export class ApplicationModule {}
