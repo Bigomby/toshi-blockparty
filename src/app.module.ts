759ac2e411f80c693e695946e0d8687a3969170c
Diego Fernández Barrera <bigomby@gmail.com>
2018-03-23 16:30:54 +0100

    :fire: Clean unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ClientModule } from './modules/client/client.module';
 import { IdentityModule } from './modules/identity/identity.module';
 import { EthModule } from './modules/eth/eth.module';
 import { SessionModule } from './modules/session/session.module';
-import { WSModule } from './modules/ws/ws.module';
 import { WalletModule } from './modules/wallet/wallet.module';
 
 @Module({
@@ -13,7 +12,6 @@ import { WalletModule } from './modules/wallet/wallet.module';
     IdentityModule,
     SessionModule,
     WalletModule,
-    WSModule,
   ],
 })
 export class ApplicationModule {}
