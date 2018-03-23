edc1b3e0a1760607029e72d846dac1cd582dc399
Diego Fernández Barrera <bigomby@gmail.com>
2018-03-23 16:30:24 +0100

    :sparkles: Implement basic wallet service to obtain Payments IDs

--- src/modules/wallet/wallet.service.ts
+++ src/modules/wallet/wallet.service.ts
@@ -7,6 +7,14 @@ export class WalletService {
   private readonly identityKey = Config.TOKEN.IDENTITY_KEY;
   private readonly paymentKey = Config.TOKEN.PAYMENT_KEY;
 
+  public getIdentityAddress(): string {
+    return this.identityKey.address;
+  }
+
+  public getPaymentAddress(): string {
+    return this.paymentKey.address;
+  }
+
   public sign(data): string {
     return this.identityKey.sign(data);
   }
